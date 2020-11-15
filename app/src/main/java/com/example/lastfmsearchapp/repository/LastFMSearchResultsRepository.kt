7d216402d40947fa457e953d6c4e6a51562455fb
Ravisara S. Kariyawasam <ravisara@gmail.com>
2020-11-15 12:12:01 +0000

    Code from the repository copied over to the Viewmodel for the time being to keep things simple. Comments out the old code.

--- app/src/main/java/com/example/lastfmsearchapp/repository/LastFMSearchResultsRepository.kt
+++ app/src/main/java/com/example/lastfmsearchapp/repository/LastFMSearchResultsRepository.kt
@@ -1,5 +1,10 @@
 package com.example.lastfmsearchapp.repository
 
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+
 /*API key: 2f6d1c3a77d7364fe9c8b7d955a56e11
 
 Shared secret: 6f2327ff8b4d7ca6813f250b4aa35984
@@ -12,8 +17,26 @@ class LastFMSearchResultsRepository {
 
     //http://ws.audioscrobbler.com/2.0/?method=album.search&album=believe&api_key=YOUR_API_KEY&format=json
     suspend fun fetchSearchResults(keyWordsToSearchFor: String) {
-       val ok = OkHttpClient()
+        // Instantiate the RequestQueue.
+/*        val queue = Volley.newRequestQueue()
+
+        val url = "https://www.google.com"
+
+// Request a string response from the provided URL.
+        val stringRequest = StringRequest(Request.Method.GET, url,
+                Response.Listener<String> { response ->
+                    // Display the first 500 characters of the response string.
+                    textView.text = "Response is: ${response.substring(0, 500)}"
+                },
+                Response.ErrorListener { textView.text = "That didn't work!" })
+
+
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest)
+
+
+    }*/
 
     }
-
 }
